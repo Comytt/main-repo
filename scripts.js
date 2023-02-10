2c828faba28ee6ba43a7f742ef72429fb09cd78b
Comytt <78920674+Comytt@users.noreply.github.com>
2023-02-10 12:48:28 -0500

    new nav bar

--- scripts.js
+++ scripts.js
@@ -1,14 +1,5 @@
-(function(d,t) {
-        var BASE_URL="https://app.chatwoot.com";
-        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-        g.src=BASE_URL+"/packs/js/sdk.js";
-        g.defer = true;
-        g.async = true;
-        s.parentNode.insertBefore(g,s);
-        g.onload=function(){
-          window.chatwootSDK.run({
-            websiteToken: 'KGJkHXJu33fxgizsXcG2Jpqh',
-            baseUrl: BASE_URL
-          })
-        }
-      })(document,"script");
+$(document).ready(function(){
+  $('.toggle').click(function(){
+      $('.menu').toggleClass('active');
+  });
+});
