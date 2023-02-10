6380eee951fb7e1400b31f62785657d2c89ad703
Comytt <78920674+Comytt@users.noreply.github.com>
2023-02-10 13:29:07 -0500

    test

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-  $('.menuicon').click(function(){
-      $('.menu').toggleClass('active');
+$(document).ready(function() {
+  // Transition effect for navbar 
+  $(window).scroll(function() {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if($(this).scrollTop() > 500) { 
+        $('.navbar').addClass('solid');
+    } else {
+        $('.navbar').removeClass('solid');
+    }
   });
 });
